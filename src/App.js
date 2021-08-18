650dafcfdfb75de29f8fd865955a328febfd9714
Gideon Odiokine <gidysmartodiokineh>
2021-08-18 01:52:26 +0100

    added my sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div>
-      <h2>Hello Decord </h2>
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./sidebar.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Gidi Programming</h3>
+        <ExpandMoreIcon />
+      </div>
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
+          </div>
+          <AddIcon className="sidebar__addChannel" />
+        </div>
+      </div>
+      <div className="sidebar__channelslist">
+          <SidebarChannel />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
