8724b7d41db8fa94b77183ad2f33271d178e8e37
Gideon Odiokine <gidysmartodiokineh>
2021-08-19 05:03:54 +0100

    firebase  SDK added

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import userReducer from "../features/news/newsSlice";
-
+import userReducer from "../features/user/userSlice";
+import appReducer from "../features/user/appSlice";
 export default configureStore({
   reducer: {
     user: userReducer,
+    app: appReducer,
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
