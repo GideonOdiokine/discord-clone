0506d3f4513d39ca62c530dc7586f9e5c3145dad
Gideon Odiokine <gidysmartodiokineh>
2021-08-20 02:49:46 +0100

    added login page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import "./App.css";
 import Chat from "./components/Chat/Chat";
 import Sidebar from "./components/sidebar/Sidebar";
-
+import { useSelector } from "react-redux";
+import { selectUser } from "../src/features/user/userSlice";
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <h2>Login Abeg(Mbok)</h2>
+      )}
     </div>
   );
 }
