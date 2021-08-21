7cffc6781429d67de4037826151eb2bd6c4e973a
Gideon Odiokine <gidysmartodiokineh>
2021-08-21 04:34:10 +0100

    added login btn to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Button } from "@material-ui/core";
+import "./login.css";
 
 function Login() {
+  const signIn = () => {
+    console.log(1234);
+  };
   return (
-    <div>
-      <h2>Loginnnf d</h2>
+    <div className="login">
+      <div className="login__logo">
+        <img
+          src="https://www.logo.wine/a/logo/Discord_(software)/Discord_(software)-Logo.wine.svg"
+          alt="logo"
+        />
+      </div>
+      <Button onClick={signIn}>Sign In</Button>
     </div>
   );
 }
